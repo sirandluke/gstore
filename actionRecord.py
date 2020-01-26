bc2d3dc23602f350e661e830949a7be34589ded9
Luke Sirand <50196820+sirandluke@users.noreply.github.com>
2020-01-26 14:27:41 -0800

    consolidate addRecord, delete, look up into 1 script

--- actionRecord.py
+++ actionRecord.py
@@ -3,8 +3,6 @@
 # Intended for use at The General Store Cooperative UCSD
 #########
 
-#TODO: IMPORT USER INPUT FROM MAIN!!!!
-
 #TODO: "insert or update" in peewee. If something is a dublicate, it will update entry instead of error
 
 import pymysql
@@ -15,23 +13,7 @@ import recordMain
 
 print("actionRecord is now running")
 
-# try:
-#     cnx = mysql.connector.connect(user='#', password='#', host='#', database='#')
-
-# except mysql.connector.Error as err:
-#     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-#         print("Something is wrong with your user name or password")
-#     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-#         print("Database does not exist")
-#     else:
-#         print(err)
-# else:
-#   cnx.close()    
-
-# cursor = cnx.cursor()
-
-
-myDB = pw.MySQLDatabase("gstoredata",host="#",port=#####,user="###",passwd="####")
+myDB = pw.MySQLDatabase("gstoredata",host="gstore.cehnumckhx7r.us-east-2.rds.amazonaws.com",port=3306,user="admin",passwd="Villeneuve")
 
 # create Base Model Record
 class BaseModel(pw.Model):
@@ -43,15 +25,14 @@ class BaseModel(pw.Model):
 class Record(BaseModel):
     album = pw.PrimaryKeyField()
     artist = pw.CharField()
-    price = pw.IntegerField()
+    price = pw.DecimalField()
     label = pw.CharField()
     class Meta:
         table_name = 'records'
 
 myDB.connect()
-
-
- # add Record to database
+ 
+# add Record to database
 def insert(album,artist,price,label):
     print("inserting")
     #check if fields are valid here
@@ -63,16 +44,18 @@ def insert(album,artist,price,label):
     }).execute()
     print("Item has been added!")
 
-#delete record from database
+# delete record from database using key: album
 def delete(album):
     print("deleting")
     res = Record.get_by_id(album)
+    # check if album is in the database
     if (res == album):
-        print(res)
-        res.delete_instance()
+        res = Record.get(Record.album == album)
+        res.delete_instance() # returns the number of rows deleted
     else:
         print("Item not found!")
 
+# look for item in database using key: album
 def lookup(album):
     print("searching")
     res = Record.get_by_id(album)
